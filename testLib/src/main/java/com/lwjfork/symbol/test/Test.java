a7eb0bcc603f1aca2417c7f309c2564d2f4bf915
liuwenjie1@ybm100.com <lwjfork@gmail.com>
2019-12-15 13:48:01 +0800

    写入stringTable信息 方便分析信息

--- testLib/src/main/java/com/lwjfork/symbol/test/Test.java
+++ testLib/src/main/java/com/lwjfork/symbol/test/Test.java
@@ -9,15 +9,13 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.function.Consumer;
 
 // lwjfork@gmail.com
 
 public class Test {
 
 
-
-    public static String filePath = System.getProperty("user.dir")+"/testLib/src/resources/testpod";
+    public static String filePath = System.getProperty("user.dir") + "/testLib/src/resources/testpod";
 
 
     public static String outPutFilePath = System.getProperty("user.dir") + "/testLib/build/outPut";
@@ -33,24 +31,21 @@ public class Test {
         ParseIOSSymbol parseIOSSymbol = new ParseIOSSymbol();
         List<BaseSymbol> baseSymbols = parseIOSSymbol.parseSymbol(filePath);
 
-        baseSymbols.forEach(new Consumer<BaseSymbol>() {
-            @Override
-            public void accept(BaseSymbol baseSymbol) {
-                long cpuType = baseSymbol.cpuType;
-                baseSymbol.section.forEach(new Consumer<SectionContent>() {
-                    @Override
-                    public void accept(SectionContent sectionContent) {
-                        try {
-                            writeSection(cpuType, sectionContent);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-        });
+        for (BaseSymbol baseSymbol : baseSymbols) {
+            writeContent(baseSymbol);
+        }
+    }
+
+    public static void writeStringTable(String cpuType, String stringTable) throws IOException {
+        File dir = new File(outPutFilePath, cpuType);
+        if (!dir.exists()) {
+            FileUtils.forceMkdir(dir);
+        }
+        File outPutFile = new File(dir, "stringTable.txt");
+        FileUtils.writeStringToFile(outPutFile, stringTable);
 
     }
+
 
     public static void writeSection(String cpuType, SectionContent sectionContent) throws IOException {
         File dir = new File(outPutFilePath, cpuType + File.separator + sectionContent.segmentName);
@@ -63,14 +58,28 @@ public class Test {
     }
 
 
-    public static void writeSection(long cpuType, SectionContent sectionContent) throws IOException {
+    public static void writeContent(BaseSymbol symbol) throws IOException {
+        writeContent(symbol.cpuType, symbol);
+    }
+
+
+    public static void writeContent(long cpuType, BaseSymbol symbol) throws IOException {
+
+        String cpuName = "";
 
         if (cpuType == CPUType.ARM) {
-            writeSection("arm", sectionContent);
+            cpuName = "arm";
+
         } else if (cpuType == CPUType.ARM64) {
-            writeSection("arm64", sectionContent);
+            cpuName = "arm64";
         }
 
+        if (cpuName.length() > 0) {
+            writeStringTable(cpuName, symbol.stringTable.content);
+            for (SectionContent sectionContent : symbol.section) {
+                writeSection(cpuName, sectionContent);
+            }
+        }
 
     }
 
